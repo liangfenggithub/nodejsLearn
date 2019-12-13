e8c530383fe4ae9063e95feeb59986af2ba1dbce
liangfeng <liangfeng@aisteel.com>
2019-12-13 19:07:21 +0800

    增加get传值 和登录注册写入txt文本日志

--- 07.ejs/02.get_Post_ejs.js
+++ 07.ejs/02.get_Post_ejs.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const url = require("url");
 const ejs = require("ejs");
+const fs = require("fs");
 
 //字符串键值分离函数，输入字符串 "a=1&b=2",返回{a=1,b=2}
 function devideKeyAndVal(str) {
@@ -23,15 +24,45 @@ let app = http.createServer((req, res) => {
       res.write("请访问 http://127.0.0.1:9999/login 进行post传值测试，或者 http://127.0.0.1:9999/register 进行get测试");
       res.end();
       break;
-    //get传值待补充
-    // case "/register"
-    //   if(method == "get")
-    //   break;
+    case "/register":
+      //返回register 页面
+      ejs.renderFile("view/register.ejs", function (err, data) {
+        if (err) {
+          console.log("err is: ", err);
+        } else {
+          res.end(data);
+        }
+      })
+      break;
+    case "/doRegister":
+      //接受get方式传来的值
+      let getData = url.parse(req.url, true).query;
+      console.log("获取 到的 getData是：", getData);
+      //利用fs模块将注册的用户名和密码写入到文本文件中
+      fs.writeFile("./userdata.text", JSON.stringify(getData), (err) => {
+        if (err) {
+          console.log("写入注册信息失败，错误信息是：err,", err);
+        } else {
+          console.log("注册信息写入成功");
+          ejs.renderFile("./view/result.ejs", {
+            operate: "注册",
+            result: "成功",
+          }, (err, data) => {
+            if (err) {
+              console.log("读取ejs模板文件出错，err：", err);
+            } else {
+              res.end(data);
+            }
+          })
+        }
+      });
+      break;
     case "/login":
       //如果是get请求，则返回登录页面
       if (method == "get") {
         ejs.renderFile("view/login.ejs", function (err, data) {
           if (err) {
+            log
             console.log("err is :", err);
           } else {
             // console.log(data);
@@ -53,11 +84,21 @@ let app = http.createServer((req, res) => {
           if ((userData.username == "admin") && (userData.password == "123456")) {
             console.log("用户名和密码正确");
             loginFlag = true;
+            //写入登录信息
+            let time = new Date().getTime();//获取时间戳
+            fs.writeFile(".login.log", `用户${userData.username} 在${time} 登录系统成功`, (err) => {
+              if (err) {
+                console.log("写入登录日志出错，错误原因是：", err);
+              } else {
+                console.log("登录日志写入成功");
+              }
+            })
           } else {
             console.log("用户名和密码错误");
           }
 
           ejs.renderFile("view/result.ejs", {
+            operate: "登录",
             result: loginFlag
           }, function (err, data) {
             if (err) {
@@ -66,7 +107,6 @@ let app = http.createServer((req, res) => {
               res.end(data);//将结果返回
             }
           })
-
 
         })
       }
